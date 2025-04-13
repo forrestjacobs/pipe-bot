19b38632bd8a88b9cad021ccf707c0775729f825
Forrest Jacobs <forrestjacobs@gmail.com>
2025-04-13 02:56:32 +0000

    Only ignore EOFs if there's no pending text

--- main.go
+++ main.go
@@ -27,6 +27,15 @@ func dieOnError(err error, code exitErrorCode) {
 	}
 }
 
+func readLine(reader *bufio.Reader) (string, error) {
+	// Ignore EOFs if there's no pending text
+	line, err := "", io.EOF
+	for line == "" && err == io.EOF {
+		line, err = reader.ReadString('\n')
+	}
+	return line, err
+}
+
 func main() {
 	token, err := token.GetToken()
 	dieOnError(err, TokenReadError)
@@ -39,11 +48,9 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		line, err := reader.ReadString('\n')
-		// Ignore EOFs so the bot doesn't exit after the first piped command
-		if err == io.EOF {
-			continue
-		} else if err != nil {
+		line, err := readLine(reader)
+
+		if err != nil {
 			log.Println(err)
 			continue
 		}
