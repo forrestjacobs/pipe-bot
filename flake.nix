48854359463d6cf9666b665dd4929bdd953ee9eb
Forrest Jacobs <forrestjacobs@gmail.com>
2025-05-03 13:56:23 +0000

    Release v0.2.0

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
         };
         package = pkgs.rustPlatform.buildRustPackage {
           pname = "pipe-bot";
-          version = "0.1.0";
+          version = "0.2.0";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
         };
